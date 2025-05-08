5b5bc73b8c50a69cdc8d87ed64526086496e3f2a
emfo2405 <emfo2405@student.miun.se>
2025-05-08 16:15:38 +0200

    Schema skapat för tabellen

--- server.js
+++ server.js
@@ -14,3 +14,37 @@ mongoose.connect("mongodb://127.0.0.1:27017/experience").then(() => {
 }).catch((error) => {
     console.log("Error connecting to database: " + error);
 });
+
+//Schema för arbetslivserfarenheter
+const ExperienceSchema = new mongoose.Schema({
+    companyname: {
+        type: String, 
+        required: true
+    },
+
+    jobtitle: {
+        type: String, 
+        required: true
+    },
+
+    location: {
+        type: String, 
+        required: true
+    },
+
+    startdate: {
+        type: Date, 
+        required: true
+    },
+
+    enddate: {
+        type: Date, 
+        required: true
+    },
+
+    description: {
+        type: Date, 
+        required: true
+    }
+
+});
