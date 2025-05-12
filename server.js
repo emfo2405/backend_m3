d817cfd6b9b786b6c05c208f4aaac1cf105e5fb5
emfo2405 <emfo2405@student.miun.se>
2025-05-12 08:50:26 +0200

    Uppdaterad funktion för att lägga till data

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Skapa en koppling till MongoDB
 mongoose.connect("mongodb://127.0.0.1:27017/experience").then(() => {
@@ -69,14 +70,55 @@ let result = await Jobexperience.find({});
 
 //Funktion för att lägga in ny data
 app.post("/jobexperiences", async(req,res) => {
+    //Hämtar in data från webbplats
+    let companyname = req.body.cvcompanyname;
+    let jobtitle = req.body.cvjobtitle;
+    let place = req.body.cvplace;
+    let startdate = req.body.cvstartdate;
+    let enddate = req.body.cvenddate;
+    let description = req.body.cvdescription;
+
+    let newExperience = {
+        companyname: companyname,
+        jobtitle: jobtitle,
+        place: place,
+        startdate: startdate,
+        enddate: enddate,
+        description: description
+    }
+
+        //Struktur för error-meddelanden
+        let errors = {
+            message: "",
+            detail: "",
+            https_response: {
+    
+            }
+        };
+    
+        //Om inte alla fält är ifyllda visas ett felmeddelande
+        if(!companyname || !jobtitle || !location || !startdate || !enddate || !description) {
+                    //Error meddelamde
+                    errors.message = "Companyname, jobtitle, location, startdate, enddate och description måste vara ifyllda";
+                    errors.detail = "Du måste fylla i companyname, jobtitle, location, startdate, enddate och description i JSON";
+            
+                    //response kod
+                    errors.https_response.message = "Bad request";
+                    errors.https_response.code = 400;
+            
+                    res.status(400).json(errors);
+            
+                    return;
+        }
+
     try{
-        let result = await Jobexperience.create(req.body);
+        let result = await Jobexperience.create(newExperience);
 
         return res.json(result);
     } catch(error) {
         return res.status(400).json(error);
     }
-})
+});
 
 
 
